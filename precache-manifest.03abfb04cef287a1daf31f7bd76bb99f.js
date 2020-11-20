84e3a7062a011369f92411968908861a79eba4eb
rizwanishaq <rizwanishaq@gmail.com>
2020-11-20 12:49:58 +0100

    Updates

---
+++ precache-manifest.03abfb04cef287a1daf31f7bd76bb99f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19c1a99bc5e2227359b106e72d91cf0",
+    "url": "/event-detection-tensorflow/index.html"
+  },
+  {
+    "revision": "98950c5e680c439d9b43",
+    "url": "/event-detection-tensorflow/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "36e12fb596eaa8adfda6",
+    "url": "/event-detection-tensorflow/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "98950c5e680c439d9b43",
+    "url": "/event-detection-tensorflow/static/js/2.d42713cb.chunk.js"
+  },
+  {
+    "revision": "bf798927907b74f22b20fc9a02ddbe8e",
+    "url": "/event-detection-tensorflow/static/js/2.d42713cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e12fb596eaa8adfda6",
+    "url": "/event-detection-tensorflow/static/js/main.4881283b.chunk.js"
+  },
+  {
+    "revision": "645ca7e492eb45c4adaa",
+    "url": "/event-detection-tensorflow/static/js/runtime-main.72c86c8a.js"
+  }
+]);
